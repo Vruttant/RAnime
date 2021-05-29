950fdb37962aff07944f4202e5079d50d4ec73d7
Vruttant <vruttantb@gmail.com>
2021-05-29 18:13:36 +0530

    removed about page and route

--- app.py
+++ app.py
@@ -34,9 +34,6 @@ def getRandomAnimeList():
 def home():
     return render_template('home.html', title="Home", anime_list=getRandomAnimeList(), season=filter_season, year=filter_year, anime_slice=anime_list_slice)
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title="About")
 
 if __name__ == '__main__':
     app.run()
